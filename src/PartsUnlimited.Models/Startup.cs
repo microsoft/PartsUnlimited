62392312a4adda36019d13cc9d638a5f3339ef51
Elizabeth Maher <enewman@microsoft.com>
2015-05-05 11:21:44 -0700

    Enable Entity Framework 7 migrations for PartsUnlimited.Models

---
+++ src/PartsUnlimited.Models/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Data.Entity;
+
+namespace PartsUnlimited.Models
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            IConfiguration config = new Configuration()
+                        .AddJsonFile("Config.json")
+                        .AddEnvironmentVariables();
+            
+            var sqlConnectionString = config.Get("Data:DefaultConnection:ConnectionString");
+            if (!String.IsNullOrEmpty(sqlConnectionString))
+            {
+                services.AddEntityFramework()
+                        .AddSqlServer()
+                        .AddDbContext<PartsUnlimitedContext>(options =>
+                        {
+                            options.UseSqlServer(sqlConnectionString);
+                        });
+            }
+        }
+
+    }
+}
