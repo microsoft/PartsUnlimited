6a0f256624116ec9c3cf68d2d5f2f053db884846
EMaher <enewman@microsoft.com>
2016-01-05 12:35:03 -0800

    adding debug statements

--- scripts/Call-Dnu.ps1
+++ scripts/Call-Dnu.ps1
@@ -1,10 +1,11 @@
 # Temporarily, add web tools to path, if not already included.
 $pathToWebTools = "$env:VS140COMNTOOLS..\IDE\Extensions\Microsoft\Web Tools\External\"
 if (!$env:Path.Contains($pathToWebTools)){
-	Write-Debug "Adding '$pathToWebTools' to path for this session."
+	Write-Host "Adding '$pathToWebTools' to path for this session."
 	$env:Path += ";$pathToWebTools"
 }
+Write-Host "Current Path: $env:Path"
 
 #Call dnu
-Write-Debug "Executing: dnu $args"
+Write-Host "Executing: dnu $args"
 & dnu $args 
