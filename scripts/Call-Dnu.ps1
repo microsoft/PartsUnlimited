cef10c05701b1e127571aa47d7fe0e8771936238
EMaher <enewman@microsoft.com>
2016-01-05 14:14:47 -0800

    Change script to allow write-debug and write-verbose statements.

--- scripts/Call-Dnu.ps1
+++ scripts/Call-Dnu.ps1
@@ -1,11 +1,18 @@
+[CmdletBinding()]
+param($arg1, $arg2, $arg3, $arg4, $arg5, $arg6, $arg7, $arg8, $arg9, $arg10, $arg11, $arg12, $arg13, $arg14, $arg15)
+
+#Add dnu to path
+Write-Verbose "Add dnu to path for this session."
+Invoke-Expression "$env:USERPROFILE\.dnx\bin\dnvm.ps1 use default"
+
 # Temporarily, add web tools to path, if not already included.
 $pathToWebTools = "$env:VS140COMNTOOLS..\IDE\Extensions\Microsoft\Web Tools\External\"
 if (!$env:Path.Contains($pathToWebTools)){
-	Write-Host "Adding '$pathToWebTools' to path for this session."
+	Write-Verbose "Adding '$pathToWebTools' to path for this session."
 	$env:Path += ";$pathToWebTools"
 }
-Write-Host "Current Path: $env:Path"
+Write-Debug "Current Path: $env:Path"
 
 #Call dnu
-Write-Host "Executing: dnu $args"
-& dnu $args 
+Write-Verbose "Executing: dnu $args"
+& dnu $arg1 $arg2 $arg3 $arg4 $arg5 $arg6 $arg7 $arg8 $arg9 $arg10 $arg11 $arg12 $arg13 $arg14 $arg15 
