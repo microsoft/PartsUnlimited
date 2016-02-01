3b2153a39d45c5a26145ba89b3f18f29a1686ba3
EMaher <enewman@microsoft.com>
2016-02-01 14:02:26 -0800

    Adding missing paren

--- netci.groovy
+++ netci.groovy
@@ -24,7 +24,7 @@ def project = GithubProject
             
             // This opens the set of build steps that will be run.
             steps {
-                powerShell(readFileFromWorkspace(newJobName, buildString)
+                powerShell(readFileFromWorkspace(newJobName, buildString))
             }
 
         }
