0149c66d12456710130c0bba0bf54aa450eeed16
Victor Usoltsev <bernarden@mail.ru>
2016-11-04 14:22:06 +1300

    Feature Flag for a Mobile Solution

---
+++ src/PartsUnlimited.Mobile/PartsUnlimited/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PartsUnlimited.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<ShoppingItemViewModel> _shoppingItems;
+
+        public ObservableCollection<ShoppingItemViewModel> ShoppingItems
+        {
+            get { return _shoppingItems; }
+            set
+            {
+                _shoppingItems = value;
+                OnPropertyChanged(nameof(ShoppingItems));
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            _shoppingItems = new ObservableCollection<ShoppingItemViewModel>
+            {
+                new ShoppingItemViewModel
+                {
+                    ItemName = "Filter Set",
+                    Price = 28.99M,
+                    DiscountPercentage = 20,
+                    ImageSource = "product_oil_filters.jpg"
+                },
+                new ShoppingItemViewModel
+                {
+                    ItemName = "Oil and Filter Combo",
+                    Price = 34.49M,
+                    DiscountPercentage = 20,
+                    ImageSource = "product_oil_oil_filter_combo.jpg"
+                },
+                new ShoppingItemViewModel
+                {
+                    ItemName = "Synthetic Engine Oil",
+                    Price = 36.49M,
+                    DiscountPercentage = 20,
+                    ImageSource = "product_oil_premium_oil.jpg"
+                }
+            };
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ src/PartsUnlimited.Mobile/PartsUnlimited/ViewModels/ShoppingItemViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System.ComponentModel;
+using PartsUnlimited.Resources;
+using Xamarin.Forms;
+
+namespace PartsUnlimited.ViewModels
+{
+    public class ShoppingItemViewModel : INotifyPropertyChanged
+    {
+        private readonly string _path = Device.OnPlatform("images/", "", "Assets/images/");
+        private string _imageSource;
+        private decimal _discountPercentage;
+        private decimal _price;
+        private string _itemName;
+
+        /// <summary>
+        /// Gets or sets the image source.
+        /// </summary>
+        public string ImageSource
+        {
+            get
+            {
+                if (_imageSource.StartsWith("http://") || _imageSource.StartsWith("https://"))
+                    return _imageSource;
+                return _path + _imageSource;
+            }
+            set
+            {
+                _imageSource = value;
+                OnPropertyChanged(nameof(ImageSource));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the discount percentage.
+        /// </summary>
+        public decimal DiscountPercentage
+        {
+            get { return _discountPercentage; }
+            set
+            {
+                _discountPercentage = value;
+                OnPropertyChanged(nameof(DiscountPercentage));
+                OnPropertyChanged(nameof(PriceAfterDiscount));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the price.
+        /// </summary>
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                OnPropertyChanged(nameof(Price));
+                OnPropertyChanged(nameof(PriceAfterDiscount));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the item.
+        /// </summary>
+        public string ItemName
+        {
+            get { return _itemName; }
+            set
+            {
+                _itemName = value;
+                OnPropertyChanged(nameof(ItemName));
+            }
+        }
+
+
+        public decimal PriceAfterDiscount => Price * (100M - DiscountPercentage) / 100M;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
