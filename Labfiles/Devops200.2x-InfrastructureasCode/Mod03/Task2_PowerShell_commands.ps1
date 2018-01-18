1da9355c7008ad439be9fa4c7aca0470d6eb5558
eamonnk <eamonn_kelly@hotmail.com>
2018-01-18 16:57:32 +0000

    added mod 3 labfile

---
+++ Labfiles/Devops200.2x-InfrastructureasCode/Mod03/Task2_PowerShell_commands.ps1
@@ -0,0 +1,15 @@
+﻿# Open this file i the PowerShell ISE. Highlight each command and hit F8 to run it as required
+# You can modify the commands as needed
+
+#cmdlet to sign in to you azure subscription
+Add-AzureRmAccount
+
+#define the variable to your template file. If you placed it in mydocuments, or some other system defined folder you can use the 'getfolderpath' method as below,
+$template = [environment]::getfolderpath(“mydocuments”) +"\EmptyTemplate.json" 
+
+#otherwise you can just drop in the path here and define the variable that way, as below
+$template = "C:\Labfiles\Devops200.2x-InfrastructureasCode\Mod03" +"\EmptyTemplate.json"
+
+
+# Command to deploy the ARM template
+New-AzureRmResourceGroupDeployment -ResourceGroupName 'DevOpsLab1RG' -Mode Complete -TemplateFile $template -Force
