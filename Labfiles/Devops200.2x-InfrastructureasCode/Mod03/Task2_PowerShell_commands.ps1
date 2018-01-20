ceb2ce1c3653e78b7914eef153e96d9e73cf563a
eamonnk <eamonn_kelly@hotmail.com>
2018-01-20 01:06:12 +0000

    updated lab files m03

--- Labfiles/Devops200.2x-InfrastructureasCode/Mod03/Task2_PowerShell_commands.ps1
+++ Labfiles/Devops200.2x-InfrastructureasCode/Mod03/Task2_PowerShell_commands.ps1
@@ -1,13 +1,14 @@
-﻿# Open this file i the PowerShell ISE. Highlight each command and hit F8 to run it as required
+﻿# Open this file in the PowerShell ISE. Highlight each command and hit F8 to run it as required
 # You can modify the commands as needed
 
 #cmdlet to sign in to you azure subscription
 Add-AzureRmAccount
 
-#define the variable to your template file. If you placed it in mydocuments, or some other system defined folder you can use the 'getfolderpath' method as below,
+#define a variable to point to your template file. You can run the command in either line 9 or line 12. If you placed it in 'mydocuments' folder, or some other system defined folder you can use the 'getfolderpath' method as below,
+#otherwise you can just drop in the path as in line 12 below and define the variable that way, and not run this command in line 9.
 $template = [environment]::getfolderpath(“mydocuments”) +"\EmptyTemplate.json" 
 
-#otherwise you can just drop in the path here and define the variable that way, as below
+#if you didn't drop your template file in a system folder just point to the folder path as below, and do not run the command in line 9
 $template = "C:\Labfiles\Devops200.2x-InfrastructureasCode\Mod03" +"\EmptyTemplate.json"
 
 
