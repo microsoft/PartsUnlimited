a37f75841cdde162537c313400851302882de01d
Houssem Dellai <houssem.dellai.ext@geodis.com>
2018-03-12 01:55:15 +0100

    Connected to Azure ApplicationInsights

--- src/PartsUnlimitedWebsite/Telemetry/EmptyTelemetryProvider.cs
+++ src/PartsUnlimitedWebsite/Telemetry/EmptyTelemetryProvider.cs
@@ -3,25 +3,32 @@
 
 using System;
 using System.Collections.Generic;
+using Microsoft.ApplicationInsights;
 
 namespace PartsUnlimited.Telemetry
 {
     public class EmptyTelemetryProvider : ITelemetryProvider
     {
+        private readonly TelemetryClient _telemetry = new TelemetryClient();
+
         public void TrackEvent(string message)
         {
+            _telemetry.TrackEvent(message);
         }
 
         public void TrackEvent(string message, Dictionary<string, string> properties, Dictionary<string, double> measurements)
         {
+            _telemetry.TrackEvent(message, properties, measurements);
         }
 
         public void TrackException(Exception exception)
         {
+            _telemetry.TrackException(exception);
         }
 
         public void TrackTrace(string message)
         {
+            _telemetry.TrackTrace(message);
         }
     }
 }
