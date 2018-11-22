9cdea78b8f5faf29c5f6837fd1bd5fb3df9d2400
azureuser <azureuser@partsunlimitedmrp.local>
2018-11-22 05:05:36 +0000

    updated vsts agent deploy files

---
+++ Labfiles/Devops200.4x-ConfigMgmtForContainerizedDelivery/M01/Labs/BuildAgent/Installappsusingchocolatey.ps1
@@ -0,0 +1,18 @@
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Assign Packages to Install
+$Packages = 'googlechrome',`
+            'visualstudiocode',`
+            'git',`
+            'visualstudio2017community',`
+            'visualstudio2017-workload-azure',`
+            'visualstudio2017-workload-netweb'
+
+#Install Packages
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
+
+#Reboot
+Restart-Computer
