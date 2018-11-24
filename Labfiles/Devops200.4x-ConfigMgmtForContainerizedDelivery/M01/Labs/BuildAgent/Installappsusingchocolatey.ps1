565deeccd72ad7cdbd5590642242188aa6ee749c
azureuser <azureuser@partsunlimitedmrp.local>
2018-11-24 20:24:51 +0000

    added chocolatey install file

---
+++ Labfiles/Devops200.4x-ConfigMgmtForContainerizedDelivery/M01/Labs/BuildAgent/Installappsusingchocolatey.ps1
@@ -0,0 +1,15 @@
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Assign Packages to Install
+$Packages = 'googlechrome',`
+            'visualstudiocode',`
+
+
+#Install Packages
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
+
+#Reboot
+Restart-Computer
