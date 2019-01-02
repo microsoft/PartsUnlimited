9cc7bba8f821ff0cc700a7bdfa0ad20634aa4f56
eamonnk <eamonn_kelly@hotmail.com>
2019-01-02 16:31:12 +0000

    adding labfiles

---
+++ Labfiles/AZ-400T05_Implementing_Application_Infrastructure/M01/New Text Document.ps1
@@ -0,0 +1,7 @@
+# Install IIS.
+dism /online /enable-feature /featurename:IIS-WebServerRole
+
+# Set the home page.
+Set-Content `
+  -Path "C:\\inetpub\\wwwroot\\Default.htm" `
+  -Value "<html><body><h2>Welcome to Azure! My name is $($env:computername).</h2></body></html>"
