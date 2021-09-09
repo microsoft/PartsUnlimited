5eb65e739e11619e834c858649e0885bbebec28c
Rudiansen <housefarm.id@gmail.com>
2021-09-09 11:43:46 +0700

    added wwwroot directory in website version

---
+++ src/PartsUnlimitedWebsite/wwwroot/Scripts/site.js
@@ -0,0 +1,48 @@
+var appInsights = window.appInsights || function (config) {
+    function s(config) {
+        t[config] = function () {
+            var i = arguments;
+            t.queue.push(function () {
+                t[config].apply(t, i)
+            })
+        }
+    }
+
+    var t = {
+        config: config
+    },
+        r = document,
+        f = window,
+        e = "script",
+        o = r.createElement(e),
+        i, u;
+
+    for (o.src = config.url || "//az416426.vo.msecnd.net/scripts/a/ai.0.js", r.getElementsByTagName(e)[0].parentNode.appendChild(o), t.cookie = r.cookie, t.queue = [], i = ["Event", "Exception", "Metric", "PageView", "Trace"]; i.length;) {
+        s("track" + i.pop());
+    }
+
+    return config.disableExceptionTracking || (i = "onerror", s("_" + i), u = f[i], f[i] = function (config, r, f, e, o) {
+        var s = u && u(config, r, f, e, o);
+        return s !== !0 && t["_" + i](config, r, f, e, o), s
+    }), t
+}({
+    instrumentationKey: ApplicationInsightsInstrumentationKey
+});
+
+window.appInsights = appInsights;
+appInsights.trackPageView();;function getRecommendations(recommendationId) {
+    $.ajax({
+        url: "/Recommendations/GetRecommendations",
+        type: "GET",
+        data: { recommendationId: recommendationId },
+        success: function (data) {
+            $("#recommendations-panel").html(data);
+        },
+        error: function (error) {
+            alert("Failed to get recommendations...");
+        },
+    })
+};// Initialize any jquery-ui-datepicker elements
+$(function () {
+    $(".jquery-ui-datepicker").datepicker();
+});
---
+++ src/PartsUnlimitedWebsite/wwwroot/lib/jquery-ui-smoothness/README.md
@@ -0,0 +1,2 @@
+jquery-ui-smoothness
+====================
